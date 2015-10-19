bca1dcbc96f79aabd9ac8daaffebf2e7dd275cff
zchaudhry11 <zchaudhry11@gmail.com>
2015-10-19 00:13:58 -0400

    submission

---
+++ BAssignments/B1/B1-Navigation/Nav/Assets/GameController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    private GameObject selectObject;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //System.Collections.Generic.List<GameObject> players = new System.Collections.Generic.List<GameObject>();
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Left Click");
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                if (hit.transform.tag == "Player")
+                {
+                    
+                    Debug.Log("Player Selected");
+                    selectObject = hit.transform.gameObject;
+                    if (selectObject != null)
+                    {
+                        selectObject.tag = "Selected";
+                        selectObject.SendMessage("Select", 1);
+                    }
+
+                }
+				if(hit.transform.tag == "Cube")
+				{
+                    Debug.Log("Cube Selected");
+					selectObject = hit.transform.gameObject;
+					if(selectObject != null)
+					{
+						selectObject.SendMessage("Select");
+					}
+				}
+				if (hit.transform.tag == "Ground" && selectObject != null)
+                {
+                    Debug.Log("Player Deselected");
+                    GameObject[] deselect = GameObject.FindGameObjectsWithTag("Selected");
+                    for (int i = 0; i < deselect.Length; i++)
+                    {
+                        deselect[i].SendMessage("Deselect", 1);
+                        deselect[i].tag = "Player";
+                    }
+                }
+                if (hit.transform.tag == null)
+                {
+                    Debug.Log("no tag found");
+                }
+
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            //Debug.Log("Right Click");
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                if (hit.transform.tag != null && selectObject != null && GameObject.FindGameObjectsWithTag("Selected").Length != 0)
+                {
+                    Debug.Log("Destination Selected");
+                    GameObject[] setDest = GameObject.FindGameObjectsWithTag("Selected");
+                    for (int i = 0; i < setDest.Length; i++)
+                    {
+                        setDest[i].SendMessage("Destination", hit.point);
+                    }
+                    //selectObject.SendMessage("Destination", hit.point);
+                }
+            }
+        }
+	}
+}
---
+++ BAssignments/B1/B1-Navigation/Nav/Assets/MoveTo.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+    private NavMeshAgent navAgent;
+    //private Animator anim;
+    private bool select;
+    private bool set = false;
+    private Vector3 MoveD;
+	// Use this for initialization
+	void Start () {
+        //anim = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (select && set)
+        {
+            navAgent.destination = MoveD;
+            //anim.SetTrigger("Move");
+        }
+        if (navAgent.pathStatus == NavMeshPathStatus.PathComplete)
+        {
+            set = false;
+        }
+	}
+
+    void Select(int x)
+    {
+        Debug.Log("Player Logged");
+        select = true;
+    }
+
+    void Deselect(int x)
+    {
+        Debug.Log("Player Delogged");
+        select = false;
+    }
+
+    void Destination (Vector3 dest)
+    {
+        Debug.Log("Destination Set");
+        MoveD = dest;
+        set = true;
+    }
+    
+}
